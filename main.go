993bd12867e78abdc772e0e7adb383d6a5a2793d
Madalin Marian Porojan <100992234+porojanmadalin@users.noreply.github.com>
2024-11-17 18:18:52 +0200

    Revert "Implement Transactional Style Logging (#1)" (#2)
    This reverts commit 7a7e96e8b8f97b4256f8cbcc2d6c3d4cb999fbc0.

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"go-telemetry/pkg/logging"
-)
+import "fmt"
 
 func main() {
-	//TODO: add a transaction started/finished fn
-
-	log := logging.New()
-
-	log.Debug("Hello", map[string]any{
-		"VarInt":  1,
-		"VarStr":  "Test",
-		"VarTest": 3.14,
-	})
-	log.Warning("Hello", map[string]any{
-		"VarInt":  1,
-		"VarStr":  "Test",
-		"VarTest": 3.14,
-	})
-	log.Info("Hello", map[string]any{
-		"VarInt":  1,
-		"VarStr":  "Test",
-		"VarTest": 3.14,
-	})
-	log.Error("Hello", map[string]any{
-		"VarInt":  1,
-		"VarStr":  "Test",
-		"VarTest": 3.14,
-	})
+	fmt.Println("Hello world")
 }
